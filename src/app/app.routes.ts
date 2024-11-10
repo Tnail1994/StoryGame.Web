c49e5f8cd0037093bdf5818ce76f9cf41f3ca60b
Stefano <62762187+Tnail1994@users.noreply.github.com>
2024-11-10 18:28:33 +0100

    Initial project setup: - Adding Bootstrap to the index.html file - A main menu is required. This means that the home component is defined as the main menu, which navigates to other components. - All necessary components already created - Navigation from main menu to create new story and story listen wired together - Added environment files, because the interface to be addressed does not change. Here is a simplification #5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { StoryListComponent } from './features/story-list/story-list.component';
+import { StoryDetailsComponent } from './features/story-details/story-details.component';
+import { CreateStoryComponent } from './features/create-story/create-story.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: HomeComponent
+    },
+    {
+        path: 'story-list/:mode', component: StoryListComponent
+    },
+    {
+        path: 'stories/:title', component: StoryDetailsComponent
+    },
+
+    {
+        path: 'story/create', component: CreateStoryComponent
+    },
+];
---
+++ src/app/features/create-story/create-story.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-story',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-story.component.html',
+  styleUrl: './create-story.component.css'
+})
+export class CreateStoryComponent {
+
+}
