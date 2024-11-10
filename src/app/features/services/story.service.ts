3ef3faccf81e68d18b7d95e6bd18fd7e48eebdd1
Stefano <62762187+Tnail1994@users.noreply.github.com>
2024-11-10 19:02:15 +0100

    Userstory #2: - Fill story list component - Extend story service to get all stories with completed query param - Navigate to story details component

--- src/app/features/services/story.service.ts
+++ src/app/features/services/story.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment.development';
 import { CreateStoryRequest } from '../models/create-story-request';
 
@@ -16,4 +16,15 @@ export class StoryService {
     return this.http.post<void>(`${environment.apiBaseUrl}${this.api}`, model);
   }
 
+  getStoryNames(isCompleted?: boolean): Observable<string[]> {
+    let params = new HttpParams();
+
+    if (isCompleted) {
+      params = params.set('isCompleted', isCompleted);
+    }
+
+    return this.http.get<string[]>(`${environment.apiBaseUrl}${this.api}`, {
+      params: params
+    });
+  }
 }
--- src/app/features/story-list/story-list.component.ts
+++ src/app/features/story-list/story-list.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { StoryService } from '../services/story.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-story-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './story-list.component.html',
   styleUrl: './story-list.component.css'
 })
 export class StoryListComponent {
+
+  ongoingMode: string = 'ongoing';
+  completedMode: string = 'completed';
+
+  paramMapSub?: Subscription;
+  mode: string | null = '';
+  stories$?: Observable<string[]>;
+
+  constructor(private route: ActivatedRoute, private storyService: StoryService) { }
+
+  ngOnInit(): void {
+    this.paramMapSub = this.route.paramMap.subscribe({
+      next: (params) => {
+        this.mode = params.get('mode');
+
+        if (this.mode === this.ongoingMode) {
+          this.stories$ = this.storyService.getStoryNames(false);
+        }
+      }
+    })
+  }
 
 }
